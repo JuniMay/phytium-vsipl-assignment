20ec85c46e429d6a0af40fa31efe8c11dc1406af
Juni May <juni_may@outlook.com>
2023-07-14 10:13:46 +0800

    refactor: update verification

--- verification/main.py
+++ verification/main.py
@@ -56,45 +56,44 @@ def lfm(tau, fs, fl, bw):
     
     return signal
 
-signal_ref = lfm(7e-6, 20e6, 222e6, 6e6)
 signal = np.real(lfm(7e-6, 20e6, 222e6, 6e6))
-signal_hilbert = hilbert(signal, 11)
-signal_compressed = pulse_compress(signal_hilbert, signal_ref)
-# plt.plot(np.abs(signal_compressed), label='radar-signal-compressed-abs')
-# generate a lot of wgn from lfm and overlap them in graph to see the noise distribution
-# for i in range(100):
-#     noise = wgn(signal, 0)
-#     noise_hilbert = hilbert(noise)
-#     noise_compressed = pulse_compress(noise_hilbert, signal_ref)
+
+noise_samples = []
+
+for _ in range(5000):
+    noise = wgn(signal, 0)
+    noise = hilbert(noise)
+    noise = pulse_compress(noise, noise)
     
-#     # scatter of time and abs
-#     plt.scatter(np.arange(len(noise_compressed)), np.abs(noise_compressed), color='red', s=0.1, label='noise-compressed-abs')
-    
+    noise_samples.extend(noise)
 
-fft_len = 2 * len(signal) - 1
-fft_len = np.ceil(0.5 + np.log2(fft_len))
-fft_len = np.floor(0.5 + 2 ** fft_len)
-    
-noise = wgn(signal, 0)
-noise_hilbert = hilbert(noise)
-noise_compressed = pulse_compress(noise_hilbert, signal_ref)
+xpower = np.sum(signal ** 2) / len(signal)
+npower = np.sum(np.abs(noise_samples) ** 2) / len(noise_samples)
 
-noise_pad = np.pad(noise_hilbert, (0, int(fft_len - len(noise_hilbert))), 'constant')
-noise_fft = np.fft.fft(noise_pad, fft_len)
+# noise_samples = list(filter(lambda x: np.abs(x) > 0.1, noise_samples))
 
-signal_ref_pad = np.pad(signal_ref, (0, int(fft_len - len(signal_ref))), 'constant')
-signal_ref_fft = np.fft.fft(signal_ref_pad, fft_len)
+noise_samples = np.abs(noise_samples)
 
-noise_compressed_fft = np.fft.fft(noise_compressed, fft_len)
+mean = np.mean(noise_samples)
+var = np.var(noise_samples)
+print(mean, var)
+print(xpower, npower)
+print(np.max(noise_samples))
+plt.hist(noise_samples, bins=100)
 
+percentile_95 = np.percentile(noise_samples, 95)
+percentile_99 = np.percentile(noise_samples, 99)
 
-# plt.plot(np.real(noise_fft), label = 'noise-fft-real')
-# plt.plot(np.imag(noise_fft), label = 'noise-fft-imag')
-plt.plot(np.abs(noise_fft), label = 'noise-fft-abs')
-plt.plot(np.abs(noise_compressed_fft), label = 'noise-compressed-fft-abs')
-plt.plot(np.abs(signal_ref_fft), label = 'signal-ref-fft-abs')
-plt.legend()
+# draw a line at 95% of the samples and mark the corresponding value
+plt.axvline(percentile_95, color='k', linestyle='dashed', linewidth=1)
+min_ylim, max_ylim = plt.ylim()
+plt.text(percentile_95 * 1.1, max_ylim * 0.9, f'95% of samples are below {percentile_95:.2f}')
 
-plt.savefig('./data/noise_distribution.png')
+# 99%
+plt.axvline(percentile_99, color='k', linestyle='dashed', linewidth=1)
+min_ylim, max_ylim = plt.ylim()
+plt.text(percentile_99 * 1.1, max_ylim * 0.8, f'99% of samples are below {percentile_99:.2f}')
+
+plt.show()
 
 
